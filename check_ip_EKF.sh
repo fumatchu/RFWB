a43b1c72bf8aad8e18181ddb765935921880abbe
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2025-02-12 13:28:20 -0500

    Update check_ip_EKF.sh

--- check_ip_EKF.sh
+++ check_ip_EKF.sh
@@ -52,8 +52,8 @@ update_ip_in_file() {
     search_pattern="$2"
     new_ip="$3"
     if [ -n "$search_pattern" ]; then
+        echo -e "${YELLOW}Changing IP from $search_pattern to $new_ip in $config_file.${TEXTRESET}"
         sudo sed -i "s/$search_pattern/$new_ip/g" "$config_file"
-        echo -e "${GREEN}Updated IP in $config_file.${TEXTRESET}"
     else
         echo -e "${RED}No valid pattern found in $config_file to update.${TEXTRESET}"
     fi
@@ -71,19 +71,33 @@ check_and_update_kibana_ip() {
     elasticsearch_comma_hosts_ip=$(grep 'elasticsearch, hosts:' "$config_file" | sed -n "s/.*['\"]https:\/\/\([0-9.]*\):.*/\1/p")
 
     if [ -n "$server_host_ip" ] && [ "$interface_ip" != "$server_host_ip" ]; then
+        echo -e "${YELLOW}Detected change: server.host IP in $config_file from $server_host_ip to $interface_ip.${TEXTRESET}"
         sudo sed -i "s/server.host: $server_host_ip/server.host: $interface_ip/" "$config_file"
-        echo -e "${GREEN}Updated server.host IP in $config_file.${TEXTRESET}"
         changed=1
     fi
     if [ -n "$elasticsearch_hosts_ip" ] && [ "$interface_ip" != "$elasticsearch_hosts_ip" ]; then
+        echo -e "${YELLOW}Detected change: elasticsearch.hosts IP in $config_file from $elasticsearch_hosts_ip to $interface_ip.${TEXTRESET}"
         sudo sed -i "s|https://$elasticsearch_hosts_ip:9200|https://$interface_ip:9200|g" "$config_file"
-        echo -e "${GREEN}Updated elasticsearch.hosts IP in $config_file.${TEXTRESET}"
         changed=1
     fi
     if [ -n "$elasticsearch_comma_hosts_ip" ] && [ "$interface_ip" != "$elasticsearch_comma_hosts_ip" ]; then
+        echo -e "${YELLOW}Detected change: elasticsearch, hosts IP in $config_file from $elasticsearch_comma_hosts_ip to $interface_ip.${TEXTRESET}"
         sudo sed -i "s|https://$elasticsearch_comma_hosts_ip:9200|https://$interface_ip:9200|g" "$config_file"
-        echo -e "${GREEN}Updated elasticsearch, hosts IP in $config_file.${TEXTRESET}"
         changed=1
+    fi
+
+    # Update elasticsearch.ssl.verificationMode setting only if there's a change and it's not already set to 'certificate'
+    if [ "$changed" -eq 1 ]; then
+        current_mode=$(grep 'elasticsearch.ssl.verificationMode:' "$config_file" | awk '{print $2}')
+        if [ "$current_mode" != "certificate" ]; then
+            echo -e "${YELLOW}Updating elasticsearch.ssl.verificationMode in $config_file to certificate...${TEXTRESET}"
+            sudo sed -i 's/#elasticsearch\.ssl\.verificationMode: full/elasticsearch.ssl.verificationMode: certificate/' "$config_file"
+            if [[ $? -eq 0 ]]; then
+                echo -e "${GREEN}Successfully updated elasticsearch.ssl.verificationMode to certificate in $config_file.${TEXTRESET}"
+            fi
+        else
+            echo -e "${GREEN}elasticsearch.ssl.verificationMode is already set to certificate.${TEXTRESET}"
+        fi
     fi
 
     return $changed
@@ -98,6 +112,7 @@ check_and_update_elasticsearch_ip() {
     echo -e "${YELLOW}Validating IP addresses in $config_file...${TEXTRESET}"
     config_ip=$(grep 'network.bind_host' "$config_file" | awk -F'[][]' '{print $2}' | awk -F', ' '{print $2}' | tr -d '"')
     if [ -n "$config_ip" ] && [ "$interface_ip" != "$config_ip" ]; then
+        echo -e "${YELLOW}Detected change: network.bind_host IP in $config_file from $config_ip to $interface_ip.${TEXTRESET}"
         update_ip_in_file "$config_file" "$config_ip" "$interface_ip"
         changed=1
     fi
@@ -112,16 +127,32 @@ check_and_update_filebeat_ip() {
     changed=0
 
     echo -e "${YELLOW}Validating IP addresses in $config_file...${TEXTRESET}"
-    filebeat_host_ip=$(grep 'host:' "$config_file" | sed -n 's/.*"\([0-9.]*\):5601".*/\1/p')
+    filebeat_host_ip=$(grep 'host:' "$config_file" | sed -n  's/.*"\([0-9.]*\):5601".*/\1/p')
     filebeat_hosts_ip=$(grep 'hosts:' "$config_file" | sed -n 's/.*\["\([0-9.]*\):9200"\].*/\1/p')
 
     if [ -n "$filebeat_host_ip" ] && [ "$interface_ip" != "$filebeat_host_ip" ]; then
+        echo -e "${YELLOW}Detected change: host IP in $config_file from $filebeat_host_ip to $interface_ip.${TEXTRESET}"
         update_ip_in_file "$config_file" "$filebeat_host_ip" "$interface_ip"
         changed=1
     fi
     if [ -n "$filebeat_hosts_ip" ] && [ "$interface_ip" != "$filebeat_hosts_ip" ]; then
+        echo -e "${YELLOW}Detected change: hosts IP in $config_file from $filebeat_hosts_ip to $interface_ip.${TEXTRESET}"
         update_ip_in_file "$config_file" "$filebeat_hosts_ip" "$interface_ip"
         changed=1
+    fi
+
+    # Update ssl.verification_mode to none if IP address changed and it is not already set to none
+    if [ "$changed" -eq 1 ]; then
+        current_mode=$(grep 'ssl.verification_mode:' "$config_file" | awk '{print $2}')
+        if [ "$current_mode" != "none" ]; then
+            echo -e "${YELLOW}Updating ssl.verification_mode in $config_file to none...${TEXTRESET}"
+            sudo sed -i 's/ssl\.verification_mode: full/ssl.verification_mode: none/' "$config_file"
+            if [[ $? -eq 0 ]]; then
+                echo -e "${GREEN}Successfully updated ssl.verification_mode to none in $config_file.${TEXTRESET}"
+            fi
+        else
+            echo -e "${GREEN}ssl.verification_mode is already set to none.${TEXTRESET}"
+        fi
     fi
 
     return $changed
