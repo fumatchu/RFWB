2046a817825c42e93249369cd1418d7046767d4e
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2025-02-14 13:47:25 -0500

    Update set_external.sh

--- set_external.sh
+++ set_external.sh
@@ -5,7 +5,7 @@ GREEN="\033[0;32m"
 RED="\033[0;31m"
 YELLOW="\033[1;33m"
 TEXTRESET="\033[0m"
-
+clear
 # Ensure nmcli is installed
 if ! command -v nmcli &> /dev/null; then
     echo -e "${RED}nmcli is not installed. Please install it and try again.${TEXTRESET}"
