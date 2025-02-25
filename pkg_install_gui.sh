0a714f2f0c301059126a39c762e28f2e3d9f099d
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2025-02-25 17:02:18 -0500

    Update pkg_install_gui.sh

--- pkg_install_gui.sh
+++ pkg_install_gui.sh
@@ -14,7 +14,7 @@ fi
 install_qos() {
 echo -e "${GREEN}Installing QOS for Voice...${TEXTRESET}"
     sleep 2
-    dnf -y install bc jq 
+    dnf -y install bc jq iproute-tc
     # Define the configuration and script paths
     CONFIG_FILE="/etc/rfwb-qos.conf"
     SCRIPT_FILE="/usr/local/bin/rfwb-qos.sh"
