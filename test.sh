570e92254418d65e07fd4ff68658f76a9fac50a3
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2025-04-16 08:23:11 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,12 @@ GREEN="\033[0;32m"
 RED="\033[0;31m"
 YELLOW="\033[1;33m"
 TEXTRESET="\033[0m"
-
+prepare_dialog_environment() {
+    clear
+    reset
+    sleep 1
+    export TERM=xterm-256color
+}
 # ========= VALIDATION HELPERS =========
 validate_cidr() { [[ "$1" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}/([0-9]|[1-2][0-9]|3[0-2])$ ]]; }
 validate_ip()   { [[ "$1" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; }
@@ -4383,8 +4388,11 @@ prompt_firewall_restart() {
 
     exec 3>&-
 }
+
+
 # ========= MAIN =========
 show_welcome_screen
+prepare_dialog_environment
 network_interface_count
 detect_active_interface
 prompt_static_ip_if_dhcp
@@ -4406,6 +4414,7 @@ install_selected_services
 drop_to_cli
 configure_services
 # ========= POST INSTALLATION/CLEANUP =========
+prepare_dialog_environment
 configure_dnf_automatic
 manage_inside_dns
 update_login_console
