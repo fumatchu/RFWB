19e8ae9160adb1ed425a5543c867726956036b0d
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2025-04-29 14:37:25 -0400

    Update RFWBInstall.sh

--- RFWBInstall.sh
+++ RFWBInstall.sh
@@ -9,7 +9,7 @@ RESET="\e[0m"
 USER=$(whoami)
 MAJOROS=$(cat /etc/redhat-release | grep -Eo "[0-9]" | sed '$d')
 clear
-echo -e "[${GREEN}SUCCESS${TEXTRESET}] Rocky FirewallBuilder Bootstrap"
+echo -e "[${GREEN}SUCCESS${TEXTRESET}] Rocky FirewallBuilder ${YELLOW}Bootstrap${TEXTRESET}"
 # Checking for user permissions
 if [ "$USER" = "root" ]; then
   echo -e "[${GREEN}SUCCESS${TEXTRESET}] Running as root user."
